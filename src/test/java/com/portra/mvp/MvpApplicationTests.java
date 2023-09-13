9558b92879a3c59cc48608eeb9af7debaa6141a1
Emanuel Ricardo Sierra Muentes - Ceiba Software <emanuel.sierra@ceiba.com.co>
2023-09-12 23:19:00 -0500

    Primer commit

---
+++ src/test/java/com/portra/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portra.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
